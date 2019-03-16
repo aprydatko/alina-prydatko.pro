baa8393beb4247f5085c3f966a2eb1af0d202400
Artur Prydatko <artyrpridatko@gmail.com>
2019-03-16 11:45:03 +0200

    initial typography

---
+++ src/components/typography.js
@@ -0,0 +1,17 @@
+// Import CSS
+import "./layout.css"
+
+import '../fonts/FiraSans-Thin/stylesheet.css'
+import '../fonts/FiraSans-ExtraLight/stylesheet.css'
+import '../fonts/FiraSans-Light/stylesheet.css'
+import '../fonts/FiraSans-Regular/stylesheet.css'
+import '../fonts/FiraSans-Medium/stylesheet.css'
+import '../fonts/FiraSans-ExtraBold/stylesheet.css'
+import '../fonts/FiraSans-Bold/stylesheet.css'
+import '../fonts/FiraSans-Black/stylesheet.css'
+
+import '../fonts/Roboto-Regular/stylesheet.css'
+import '../fonts/Roboto-Bold/stylesheet.css'
+
+import '../fonts/OpenSans-Regular/stylesheet.css'
+import '../fonts/OpenSans-Bold/stylesheet.css'
