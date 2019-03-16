625dfdcba0100b6bebe4f079375934eb688bf4b5
Artur Prydatko <artyrpridatko@gmail.com>
2019-03-16 11:42:41 +0200

    Initial site config

---
+++ config.js
@@ -0,0 +1,54 @@
+module.exports = {
+    siteNavigation: {
+        menu: [
+            {
+                name: `Главная`,
+                url: `/`
+            },
+            {
+                name: `Работы`,
+                url: `/works`
+            },
+            {
+                name: `Обо мне`,
+                url: `/about-me`
+            },
+            {
+                name: `Контакты`,
+                url: `/contact`
+            }
+        ]
+    },
+    siteSocial: {
+        social: [
+            {
+                icon: `src/images/icons/facebook.svg`,
+                url: `/`,
+                name: `facebook`
+            },
+            {
+                icon: `src/images/icons/twitter.svg`,
+                url: `/`,
+                name: `twitter`
+            },
+            {
+                icon: `src/images/icons/googleplus.svg`,
+                url: `/`,
+                name: `googleplus`
+            },
+            {
+                icon: `src/images/icons/linkendin.svg`,
+                url: `/`,
+                name: `linkendin`
+            }
+        ]
+    },
+    siteContacts: {
+        phone: `050 061 21 26`,
+        email: `aprydatko@gmail.com`,
+        address: `г. Сумы, ул. Прокофьева, дом 36`
+    },
+    siteCopyright: {
+        site: `aprydatko.pro`
+    }
+}
